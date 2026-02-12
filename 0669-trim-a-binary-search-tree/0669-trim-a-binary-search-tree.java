b1f86f18be84bd95bd84b432b1342afc8b50733b
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-02-13 00:23:46 +0530

    Time: 0 ms (100%), Space: 47 MB (23.84%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.java
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.java
@@ -16,10 +16,17 @@
 class Solution {
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if(root==null)return null;
+        //case 2
+        // atfirst root is 3 goes to 0 and 4
+        //0 less than 1(low) ? yes go to right to 2
         else if(root.val < low) return trimBST(root.right,low,high);
+        //4 is higher than 3 ? yes go to left that is null
         else if(root.val>high) return trimBST(root.left,low,high);
+        //1
         root.left = trimBST(root.left,low,high);
+        //2
         root.right = trimBST(root.right,low,high);
+        // so 1<low and 2<high satisfied
         return root;
 
 
