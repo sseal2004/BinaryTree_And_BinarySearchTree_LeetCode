08e6914a044e85e7a53172b0c4c471efc351b66a
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-02-12 22:50:58 +0530

    Time: 6 ms (97.06%), Space: 47.8 MB (52.3%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -13,7 +13,9 @@ class Solution {
         if(p.val ==root.val||q.val ==root.val)return root;
         else if(p.val<root.val && q.val > root.val)return root;
         else if(p.val >root.val && q.val< root.val)return root;
+        // when 2 and 4 both are less than 6 then go left and now the root is 2
         else if(p.val< root.val && q.val < root.val)return lowestCommonAncestor(root.left,p,q);
+        //so now root is 2 so p.val = 2 > 2 and q.val =4 > 2 condition satisfied go to right
         else return lowestCommonAncestor(root.right,p,q);
     }
 }
